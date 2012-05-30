f5bdac51746c6edb042988eff7971d67ebc64005
Rodrigo F Silva <abanaunderline@hotmail.com>
2012-05-30 16:30:10 -0300

    ...

--- encurta.rb
+++ encurta.rb
@@ -6,7 +6,7 @@ class ShortUrl
 # This class store the url and the Short URL.
 #It also Generate the short_url code and save all data to a _Redis_ SET.
 
-SHORT_URL_CHARACTERS_RANGE = %{ 0 1 2 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} #This constant describes the character range of the characters that for the short_url code.   
+SHORT_URL_CHARACTERS_RANGE = %w{ 0 1 2 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} #This constant describes the character range of the characters that for the short_url code.   
 attr_reader :url, :short_url, :generate_short_url
   
   def initialize(raw_url,url = 'a')
@@ -27,13 +27,23 @@ attr_reader :url, :short_url, :generate_short_url
   def generate_short_url
     urldb = Redis.new
     list =  Array.new
-    urldb.set("url_list", '{"json_class":"ShortUrl","data":{"url":"http://foo.bar","short_url":"a"}}') if urldb.get("url_list") == nil    
+    next_character = Array.new
+    new_short_url = String.new
+    urldb.set("url_list", '{"json_class":"ShortUrl","data":{"url":"http://foo.bar","short_url":"0"}}') if urldb.get("url_list") == nil    
     list << JSON.parse(urldb.get("url_list"))
     last_short_url = list.pop
     last_short_code = last_short_url.short_url
     list.push last_short_url
-    puts last_short_code
-    puts list.inspect
+    last_short_code.each_char do |c|
+      next_c = SHORT_URL_CHARACTERS_RANGE.index(c)
+      next_c += 1
+      next_character = SHORT_URL_CHARACTERS_RANGE.at(next_c)
+    end
+    new_short_url = last_short_code << "0" if next_character.nil?
+    if new_short_url.length > 4
+      new_short_url = '0'
+      
+    end
   end
   def remove
     urldb = Redis.new
@@ -43,5 +53,13 @@ attr_reader :url, :short_url, :generate_short_url
 end
 
 s = ShortUrl.new("http://foo.bar")
+t = {"json_class"=>"ShortUrl","data"=> {"url"=>"http://foo.bar","short_url"=>"0"}}.to_json
+puts t
+u = JSON.parse(t)
+v = s.to_json
+puts v
+v = JSON.parse(v)
+puts v
+puts u.inspect
 s.generate_short_url
 #s.remove
