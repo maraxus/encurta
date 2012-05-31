f9e8905a252fbefb0e8ef527fc7408ca0fb0f49c
Rodrigo F Silva <abanaunderline@hotmail.com>
2012-05-31 12:08:41 -0300

    more progress on the lists

--- encurta.rb
+++ encurta.rb
@@ -9,7 +9,7 @@ class ShortUrl
 SHORT_URL_CHARACTERS_RANGE = %w{ 0 1 2 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} #This constant describes the character range of the characters that for the short_url code.   
 attr_reader :url, :short_url, :generate_short_url
   
-  def initialize(raw_url,url = 'a')
+  def initialize(raw_url,url = '0')
     @url       = raw_url
     @short_url = url
   end
@@ -26,15 +26,16 @@ attr_reader :url, :short_url, :generate_short_url
   end
   def generate_short_url
     urldb = Redis.new
-    list =  Array.new
     next_character = Array.new
     new_short_url = String.new
-    urldb.rpush("url_list", {"json_class"=>"ShortUrl","data"=>{"url"=>"http://foo.bar","short_url"=>"0"}}.to_json.gsub!(%r[\"],"\"")) if urldb.llen "url_list" == 0
-    list << JSON.parse(urldb.get("url_list"))
-    last_short_url = list.pop
-    puts last_short_url.inspect
+    if urldb.llen "url_list" == 0
+      urldb.rpush("url_list", {"json_class"=>"ShortUrl","data"=>{"url"=>"http://foo.bar","short_url"=>"0"}}.to_json.gsub!(%r[\"],"\""))
+      urldb.incr("url_counter")
+    end
+    last_short_url = JSON.parse(urldb.rpop("url_list"))
+    urldb.rpush("url_list", last_short_url.to_json.gsub!(%r[\"],"\""))
+    puts "last short url: " + last_short_url.inspect
     last_short_code = last_short_url.short_url
-    list.push last_short_url
     last_short_code.each_char do |c|
       next_c = SHORT_URL_CHARACTERS_RANGE.index(c)
       next_c += 1
@@ -43,12 +44,14 @@ attr_reader :url, :short_url, :generate_short_url
     new_short_url = last_short_code << "0" if next_character.nil?
     if new_short_url.length > 4
       new_short_url = '0'
-      
+      urldb.del "url_counter"
+      urldb.incr "url_counter"
     end
   end
   def remove
     urldb = Redis.new
     urldb.del "url_list"
+    urldb.del "url_counter"
     puts urldb.dbsize
   end
 end
