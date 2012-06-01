8a79913ed78755c9558b8199eeebd4ca29e0caeb
Rodrigo F Silva <abanaunderline@hotmail.com>
2012-06-01 02:12:51 -0300

    Finished to implement functionality. the url_retriever class retrieve the original url from it's short url code. Refactoring is needed.

--- encurta.rb
+++ encurta.rb
@@ -1,12 +1,12 @@
 require "JSON"
 require "redis"
-
+SHORT_URL_CHARACTERS_RANGE = %w{ 0 1 2 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} #This constant describes the character range of the characters that for the short_url code.
 class ShortUrl
 # = Description 
 # This class store the url and the Short URL.
 #It also Generate the short_url code and save all data to a _Redis_ SET.
 
-SHORT_URL_CHARACTERS_RANGE = %w{ 0 1 2 4 5 6 7 8 9 0 a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} #This constant describes the character range of the characters that for the short_url code.   
+   
 attr_reader :url, :short_url, :generate_short_url
   
   def initialize(raw_url,url = '0')
@@ -55,17 +55,28 @@ attr_reader :url, :short_url, :generate_short_url
     urldb = Redis.new
     @short_url = generate_short_url
     urldb.rpush "url_list", self.to_json
-    puts "regiters: "
+    puts "registers: "
     puts urldb.llen("url_list")
   end
   def remove
     urldb = Redis.new
     urldb.del "url_list"
-    urldb.del "url_counter"
     return urldb.dbsize
   end
 end
-
+class UrlRetriever
+  def retrieve_url(short_url)
+    urldb = Redis.new
+    url_index = 0
+    short_url_code = short_url
+    short_url_code.each_char do |c|
+      url_index += SHORT_URL_CHARACTERS_RANGE.index c
+    end
+    shortened_url = JSON.parse(urldb.lindex("url_list",  url_index))
+    puts "Original url: " + shortened_url.url
+    return shortened_url.url
+  end
+end
 s = ShortUrl.new("http://foo.bar")
 t = "{\"json_class\":\"ShortUrl\",\"data\": {\"url\":\"http://foo.bar\",\"short_url\":\"0\"}}"
 puts t
@@ -76,4 +87,6 @@ u = JSON.parse(t)
 #puts u.inspect
 #puts v.inspect
 s.save
+v = UrlRetriever.new
+v.retrieve_url "5"
 #s.remove
